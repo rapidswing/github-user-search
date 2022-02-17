8b496acb223c2bbb9b88fd548114ad9b92af05f0
Bradley Wright <bradley.wright@gmail.com>
2022-02-17 15:32:31 -0500

    TitleBar styling

--- src/components/TitleBar/TitleBar.jsx
+++ src/components/TitleBar/TitleBar.jsx
@@ -4,11 +4,23 @@ import IconMoon from 'assets/icon-moon.svg'
 import IconSun from 'assets/icon-sun.svg'
 
 const Title = styled.div`
-  
+  font-size: 2.6rem;
+  font-weight: bold;
+  line-height: 3.9rem;
 `
 
 const ToggleThemeButton = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  font-size: 1.3rem;
+  font-weight: bold;
+  letter-spacing: 0.25rem;
+  line-height: 1.9rem;
 
+  img {
+    margin-left: 1.6rem;
+  }
 `
 
 const StyledTitleBar = styled.div`
