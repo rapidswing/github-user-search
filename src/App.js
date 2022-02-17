f1a32cb559c714b555d6cd65c7306810bc61ff8a
Bradley Wright <bradley.wright@gmail.com>
2022-02-17 12:54:22 -0500

    styled stubs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { createGlobalStyle } from 'styled-components'
+import styled from 'styled-components'
+
+import theme from 'theme/theme.json'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  
+  html, body, #root {
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+  }
+`
+
+const Container = styled.div`
+  margin: auto;
+`
+
 export default function App() {
   return (
-    <></>    
+    <>
+      <GlobalStyle />
+      <Container>
+        
+      </Container>
+    </>
   )
 }
