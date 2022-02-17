adca6f7d7ae8bc0bb20a234efd7372637f4b0086
Bradley Wright <bradley.wright@gmail.com>
2022-02-17 18:25:50 -0500

    Mobile styling done for SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,34 +2,32 @@ import styled from 'styled-components'
 
 import IconSearch from 'assets/icon-search.svg'
 
-const UnstyledSearchBar = ({ className }) => {
-  return (
-    <div className={className}>
-      <SearchIcon>
-        <img src={IconSearch} alt="Search" />
-      </SearchIcon>
-      
-      <input type="text" placeholder="Search GitHub username…" />
-      <button>Search</button>
-    </div>
-  )
-}
+const UnstyledSearchBar = ({ className, children }) => (
+  <div className={className}>
+    <SearchIcon>
+      <img src={IconSearch} alt="Search" />
+    </SearchIcon>
+    <input type="text" placeholder="Search GitHub username…" />
+    <button>Search</button>
+  </div>
+)
 
 const SearchBar = styled(UnstyledSearchBar)`
   align-items: center;
-  background: ${(props) => props.theme.colors.almostWhite};
+  background: ${props => props.theme.colors.searchBg};
   border-radius: 0.7rem;
   display: grid;
   grid-template-columns: auto 1fr auto;
 
   button {
-    background: ${props => props.theme.colors.brightBlue};
+    background: ${props => props.theme.colors.searchBtnBg};
     border: none;
     border-radius: 1rem;
-    color: ${props => props.theme.colors.white};
+    color: ${props => props.theme.colors.searchBtnText};
     font-family: ${props => props.theme.font};
-    font-size: 1.4rem;
-    font-weight: bold;
+    font-size: 1.2rem;
+    font-weight: 700;
+    letter-spacing: 0.12rem;
     line-height: 2.1rem;
     margin: 0.7rem;
     padding: 1.25rem 1.4rem 1.25rem 1.8rem;
@@ -42,10 +40,21 @@ const SearchBar = styled(UnstyledSearchBar)`
 
   input {
     border: none;
+    color: ${props => props.theme.colors.searchText};
     font-family: ${props => props.theme.font};
     font-size: 1.3rem;
     line-height: 2.5rem;
-    margin-left: 0.9rem;
+    margin-left: 0.7rem;
+
+    ::placeholder,
+    ::-webkit-input-placeholder {
+      color: ${props => props.theme.colors.searchPlaceholder};
+      opacity: 0.4;
+    }
+    :-ms-input-placeholder {
+      color: ${props => props.theme.colors.searchPlaceholder};
+      opacity: 0.4;
+    }
   }
 `
 
